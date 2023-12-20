263d746d07a20dd3447b614a71377b387e120482
platinumpizza29 <kbelgi1gmail.com>
2023-12-20 10:54:41 +0000

    headers added

--- functions/openai/index.ts
+++ functions/openai/index.ts
@@ -1,45 +1,47 @@
-import { CreateCompletionRequest } from 'https://esm.sh/openai@3.1.0'
+import { CreateCompletionRequest } from "https://esm.sh/openai@3.1.0";
 
 const corsHeaders = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
-}
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers":
+    "authorization, x-client-info, apikey, content-type"
+};
 
-Deno.serve(async (req) => {
+Deno.serve(async req => {
   try {
-    const { query } = await req.json()
+    const { query } = await req.json();
 
     const completionConfig: CreateCompletionRequest = {
-      model: 'gpt-3.5-turbo-instruct',
+      model: "gpt-3.5-turbo-instruct",
       prompt: query,
       max_tokens: 256,
       temperature: 0,
-      stream: false,
+      stream: false
+    };
+
+    if (req.method === "OPTIONS") {
+      return new Response("ok", { headers: corsHeaders });
     }
 
-    if (req.method === 'OPTIONS') {
-      return new Response('ok', { headers: corsHeaders })
-    }
-
-    const res = fetch('https://api.openai.com/v1/completions', {
-      method: 'POST',
+    const res = fetch("https://api.openai.com/v1/completions", {
+      method: "POST",
       headers: {
-        Authorization: `Bearer ${Deno.env.get('OPENAI_API_KEY')}`,
-        'Content-Type': 'application/json',
+        ...corsHeaders,
+        Authorization: `Bearer ${Deno.env.get("OPENAI_API_KEY")}`,
+        "Content-Type": "application/json"
       },
-      body: JSON.stringify(completionConfig),
-    })
+      body: JSON.stringify(completionConfig)
+    });
 
     return new Response(JSON.stringify((await res).body), {
-      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
-    })
+      headers: { ...corsHeaders, "Content-Type": "application/json" }
+    });
   } catch (error) {
     return new Response(JSON.stringify({ error: error.message }), {
-      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
-      status: 400,
-    })
+      headers: { ...corsHeaders, "Content-Type": "application/json" },
+      status: 400
+    });
   }
-})
+});
 
 /* To invoke locally:
 
